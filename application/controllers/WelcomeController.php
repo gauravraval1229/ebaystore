770acd5643f447bdd33fd5a853af0c7eb3cddb3c
unknown <priyu08@gmail.com>
2020-05-23 16:29:46 +0530

    token comment

--- application/controllers/WelcomeController.php
+++ application/controllers/WelcomeController.php
@@ -13,7 +13,7 @@ class WelcomeController extends CI_Controller {
         $this->load->model('UserModel','userModel');
         $this->load->helper('url');
         $this->checklogintoken->checkLogin(); // check user is loggedin or not
-        $this->checklogintoken->checkToken(); //token expired or not
+        //$this->checklogintoken->checkToken(); //token expired or not
 
         include_once (APPPATH.'libraries/amazon/feed/Client.php');
         include_once (APPPATH.'libraries/amazon/feed/Interface.php');
